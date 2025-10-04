065de66bed88b1ed4a45c19a251f6337026004fe
aFang <15342690689@139.COM>
2025-10-04 18:02:35 +0800

    修复backend
    后端环境修复

---
+++ backend/afang-user-backend/src/test/java/com/example/afanguserbackend/AfangUserBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.afanguserbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfangUserBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
